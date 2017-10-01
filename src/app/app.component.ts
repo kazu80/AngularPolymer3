0ed2265eec5973d0ccfaa6aff54e63a0937d0c1d
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-10-01 09:34:32 +0900

    Angular with Polymer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+    // templateUrl: './app.component.html',
+    template: `
+        ccc{{isClick}}
+        <kazu-polymer [(isClick)]="isClick"></kazu-polymer>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title   = 'app';
+    isClick = false;
 
-    foo() {
-        console.log('I am app.component.ts');
+    click() {
+        // this.isClick = !this.isClick;
     }
 }
