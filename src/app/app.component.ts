7f7f5c08eb8bc5a1c352d833bd6f45de95e52aff
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-10-02 18:17:58 +0900

    Angular with Polymer fire event from polymer. count up / down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-    // templateUrl: './app.component.html',
-    template: `
-        <kazu-polymer id="foo"></kazu-polymer>
-    `,
-  styleUrls: ['./app.component.css']
+  selector     : 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    title   = 'app';
+    title = 'app';
+    num   = 0;
 
     ngOnInit(): void {
+        const up_down = document.getElementById('up-down');
+
+        up_down.addEventListener('up_val', (e) => {
+            if (this.num < 10) {
+                this.num = this.num + 1;
+            }
+        });
+
+        up_down.addEventListener('down_val', (e) => {
+            if (this.num > 0) {
+                this.num = this.num - 1;
+            }
+        });
+
+        /*
         const kazu = document.getElementById('foo');
 
         kazu.addEventListener('kick', function (e) {
             console.log('kick!!!');
         });
+        */
     }
 }
